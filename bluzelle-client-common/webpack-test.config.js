5da169d2b90c5d0538af62262bfc2db4ff0f8c83
Monty Thibault <monty@bluzelle.com>
2018-05-08 10:11:17 -0700

    Isolated CRUD app

--- bluzelle-client-common/webpack-test.config.js
+++ bluzelle-client-common/webpack-test.config.js
@@ -1,17 +1,15 @@
 const webpack = require('webpack');
 
-const config = require('bluzelle-client-common/webpack-base-config.js')();
 const path = require('path');
 
-config.entry = {test: path.resolve('./test.js')};
 
-config.resolve.alias = Object.assign({
-    'bluzelle-client-common/services/CommunicationService': path.resolve('./mocks/MockCommunicationService')
-}, config.resolve.alias);
+const config = require(path.resolve('./webpack.config.js'));
+
+
+config.entry = {test: path.resolve('./test.js')};
 
 config.plugins.push(new webpack.ProvidePlugin({
     waitFor: 'promise-waitfor'
 }));
 
 module.exports = config;
-
